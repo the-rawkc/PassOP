6c977130c41683ef7d01c2b504634bb8bcad3825
Ravi Mohan Rohela <168910346+the-rawkc@users.noreply.github.com>
2024-11-25 18:30:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Manager from './components/Manager'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Manager from './components/Manager';
+import Footer from './components/Footer';
+import Login from './components/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Navbar />
-    <div className='min-h-[81vh]'>
-    <Manager />
-    </div>
-    <Footer />
-    </>
-  )
+    <Router>
+      {/* Navbar is placed outside Routes to make it persistent across pages */}
+      <Navbar />
+      <div className="min-h-[81vh]">
+        <Routes>
+          {/* Define the routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/manager" element={<Manager />} />
+        </Routes>
+      </div>
+      {/* Footer is also persistent */}
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
